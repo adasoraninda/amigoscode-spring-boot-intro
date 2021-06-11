2f6965c1c50fc70519cfca9c85724a3063a3018d
adasoraninda <adasoraninda@gmail.com>
2021-06-11 23:11:27 +0700

    Add configuration

---
+++ src/main/java/com/adasoraninda/demo/student/StudentConfig.java
@@ -0,0 +1,24 @@
+package com.adasoraninda.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student adasoraninda = new Student("adasoraninda", "adasoraninda@gmail.com", LocalDate.of(2000, Month.JANUARY, 21), 21);
+       
+            Student alex = new Student("alex", "alex@gmail.com", LocalDate.of(2002, Month.JANUARY, 21), 18);
+            
+            repository.saveAll(List.of(adasoraninda, alex));
+        };
+    }
+
+}
