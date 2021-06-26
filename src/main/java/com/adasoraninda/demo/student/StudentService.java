b6125dc30fae27bff0ef7d5ff14dd5b8239845fb
adasoraninda <adasoraninda@gmail.com>
2021-06-26 22:05:25 +0700

    Add some business logic

--- src/main/java/com/adasoraninda/demo/student/StudentService.java
+++ src/main/java/com/adasoraninda/demo/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.adasoraninda.demo.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,13 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
     }
 
 }
