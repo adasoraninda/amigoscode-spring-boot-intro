960e622f149114f97a22bd9aaf0e688cc2fe28ea
adasoraninda <adasoraninda@gmail.com>
2021-06-26 22:45:43 +0700

    Add Put request

--- src/main/java/com/adasoraninda/demo/student/StudentService.java
+++ src/main/java/com/adasoraninda/demo/student/StudentService.java
@@ -1,7 +1,10 @@
 package com.adasoraninda.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +21,6 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
-
     }
 
     public void addNewStudent(Student student) {
@@ -38,6 +40,26 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("student with id " + studentId + "does not exists"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+
+            student.setEmail(name);
+        }
     }
 
 }
